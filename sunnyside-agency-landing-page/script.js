60b095d823c717a9a2925c279e84e022864f2a0b
hunjager <59488679+hunjager@users.noreply.github.com>
2024-06-11 09:33:55 +0100

    Add files via upload

---
+++ sunnyside-agency-landing-page/script.js
@@ -0,0 +1,5 @@
+const drop = document.getElementById("drop")
+const display = document.querySelector(".inner-contact");
+drop.addEventListener("click",()=>{
+    display.classList.toggle("show")
+})
